70d2944dc55e066d02a1e5fd1bacf52d50642f29
huangyangjie <huang.yj@founder.com.cn>
2025-06-15 22:37:38 +0800

    添加其它平台

--- lib/configs/api_config.dart
+++ lib/configs/api_config.dart
@@ -1,6 +1,9 @@
 class ApiConfig {
-  static const String apiKey = 'sk-***************************'; // sk-****************
-  static const String baseUrl = 'https://api.deepseek.com'; // "https://dashscope.aliyuncs.com/compatible-mode/v1"
+  static const String apiKey =
+      'sk-1eb782b2a2014e3ab27a16952da741bd'; // sk-****************
+  static const String baseUrl =
+      'https://api.deepseek.com'; // "https://dashscope.aliyuncs.com/compatible-mode/v1"
   static const String customModel = 'deepseek-chat'; // "deepseek-chat"
-  static const String deepThinkingModel = 'deepseek-reasoner'; // "deepseek-reasoner"
+  static const String deepThinkingModel =
+      'deepseek-reasoner'; // "deepseek-reasoner"
 }
